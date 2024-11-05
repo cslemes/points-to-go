ac5e1ef766d216c3ef0ab7013d726b80525d5886
Cristiano Lemes <cslemess@gmail.com>
2024-11-05 18:56:56 -0300

    deploy cd terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -37,7 +37,7 @@ resource "aws_ecs_task_definition" "task_def" {
   requires_compatibilities = ["FARGATE"]
   cpu                      = "256"
   memory                   = "512"
-  task_role_arn            = data.aws_ssm_parameter.ecsTaskExecutionRole.value
+
 
   container_definitions = jsonencode([{
     name      = "my-container"
@@ -59,7 +59,7 @@ resource "aws_ecs_service" "my_service" {
   task_definition = data.aws_ssm_parameter.cluster_name.arn
   desired_count   = 1
   launch_type     = "FARGATE"
-
+  iam_role        = data.aws_ssm_parameter.ecsTaskExecutionRole.arn
 
 
   network_configuration {
