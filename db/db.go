8a66414818f208f4c645c4fcbdedd48f9c3674e0
Cristiano Lemes <cslemess@gmail.com>
2024-11-15 12:24:07 -0300

    change db to private

--- db/db.go
+++ db/db.go
@@ -2,6 +2,7 @@ package db
 
 import (
 	"fmt"
+	"log"
 	"os"
 
 	"gorm.io/driver/mysql"
@@ -16,15 +17,15 @@ func OpenDBConnection() (*gorm.DB, error) {
 	// }
 
 	HOST_DB := os.Getenv("HOST_DB")
-	//PORT_DB := os.Getenv("PORT_DB")
+	PORT_DB := os.Getenv("PORT_DB")
 	USER_DB := os.Getenv("USER_DB")
 	PASSWORD_DB := os.Getenv("PASSWORD_DB")
 
-	//log.Println(PORT_DB)
+	log.Println(PORT_DB)
 	// UNIX dsn
-	dsn := "%s:%s@unix(/cloudsql/%s)/%s?charset=utf8mb4&parseTime=True&loc=Local"
-	//dsn := "%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local"
-	dsn = fmt.Sprintf(dsn, USER_DB, PASSWORD_DB, HOST_DB, "points")
+	//dsn := "%s:%s@unix(/cloudsql/%s)/%s?charset=utf8mb4&parseTime=True&loc=Local"
+	dsn := "%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local"
+	dsn = fmt.Sprintf(dsn, USER_DB, PASSWORD_DB, HOST_DB, PORT_DB, "points")
 
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	return db, err
